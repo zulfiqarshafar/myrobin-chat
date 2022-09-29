f6cf25c5c8206c2ba744cae0a98194480df67213
zulfiqarshafar <zul.shafar@gmail.com>
2022-09-29 11:05:15 +0800

    homepage functionality

--- backend/routes/ChatRoomRouter.js
+++ backend/routes/ChatRoomRouter.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const chatRoomController = require("../controllers/ChatRoomController.js");
 
 const router = express.Router();
 
+router.post("/join", chatRoomController.post_join);
 router.get("/:id", (req, res) => {
   console.log(req.params.id);
 });
---
+++ frontend/src/components/Alert.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="alert" :class="alertType">
+    <slot>Alert</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "alert",
+  props: {
+    alertType: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.alert {
+  border-radius: 8px;
+  padding: 16px;
+
+  &.danger {
+    border: 1px solid rgb(255, 69, 69);
+    background-color: rgba(255, 0, 0, 0.3);
+  }
+}
+</style>
